ef82f273146b5845fdc0bfc6194a132814d90166
KKKLEIN <31945072+jinking2007@users.noreply.github.com>
2025-08-17 16:28:41 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -16,14 +16,14 @@ async fn hello_world() -> &'static str {
 
 async fn setup_environment() { 
     let env_vars = [
-        ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
-        ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
+        ("UUID", "7616c382-7143-4c58-969a-59cde6e37238"),
+        ("NEZHA_SERVER", "nzng.wfly81.dpdns.org:80"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
-        ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("NEZHA_KEY", "RDYN9bmf3d9gNe8RFI3BhyeDVFCvB4gm"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
+        ("ARGO_DOMAIN", "seva.ksemo.pp.ua"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", "{"AccountTag":"fa0db1c83b99c1c20faf439627737f7d","TunnelSecret":"gn/gTGGWT4ANpIDI9EGw/BgglHX07jEKug1ct40C4f0=","TunnelID":"8b8c7a96-d919-45e5-a646-dc9d39a5ef37","Endpoint":""}"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
-        ("CFIP", "time.is"),    // 优选域名或优选ip
+        ("CFIP", "cf.090227.xyz"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
         ("NAME", "Shuttle"),    // 节点名称
         ("FILE_PATH", "./tmp"), // 运行目录，保持不变
